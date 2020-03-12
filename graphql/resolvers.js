b702158992c1bbbcfe287e36d37a99e6792004e3
최형동 <dong@choehyeongdong-ui-MacBookPro.local>
2020-03-12 13:58:17 +0900

    Extending the Schema part 1

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,6 +1,12 @@
+const Dong ={
+    name:"Hyeoung Dong",
+    age:27,
+    gender:"male"
+}
+
 const resolvers = {
     Query:{
-        name:()=>"Hyeoung Dong"
+        person:()=>Dong
     }
 }
 export default resolvers;
